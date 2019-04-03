32ec813182b307fc25f33c9102006b34613a75f3
KnightRider <omar.brown@comcast.net>
2019-04-03 12:05:17 -0400

    3rd changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//first problem
 for(let x = 0; x <= 10; x++){
 let v = "v v v v v v v v v v";
  
@@ -6,11 +6,19 @@ let v = "v v v v v v v v v v";
 
 }
 
-
+//Second Problem
 let boots = ['v', 'vv', 'vvv','vvvv','vvvvv','vvvvvv','vvvvvvv', 'vvvvvvvv', 'vvvvvvvvvv','vvvvvvvvvvv']
-for (var i = 0; i < boots.length; i+=2) {
+for (var i = 0; i < boots.length; i++) {
   console.log(boots[i])
 }
+//Third Problem
+let pyramid = ['     v', '    vvv', '   vvvvv','  vvvvvvv',' vvvvvvvvv']
+for (var i = 0; i < pyramid.length; i++) {
+  console.log(pyramid[i])
+}
+//Fourth Problem
+let cal = ['Su Mo Tu We Th Fr Sa', '    1  2  3  4  5  6', ' 7  8  9 10 11 12 13','14 15 16 17 18 19 20','21 22 23 24 25 26 27 ','28 29 30 31   ']
+for (var i = 0; i < cal.length; i++) {
+  console.log(cal[i])
 
-
-
+}
